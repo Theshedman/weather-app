9b2434281de15869af9bd26aed0d2a12ccbcf4d8
Shedrack Ajaegbu <shedrack3@gmail.com>
2019-07-25 01:44:40 +0100

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,11 +12,10 @@ const forecast = (lat, long, callback) => {
     } else {
       const { temperature, precipProbability } = body.currently;
       const { data } = body.daily;
+      const { summary, temperatureHigh, temperatureLow } = data[0];
       callback(
         undefined,
-        `${
-          data[0].summary
-        } It is currently ${temperature} degree out. There is a ${precipProbability}% chance of rain.`,
+        `${summary} It is currently ${temperature} degree out. The high today is ${temperatureHigh} with a low of ${temperatureLow}. There is a ${precipProbability}% chance of rain.`,
       );
     }
   });
